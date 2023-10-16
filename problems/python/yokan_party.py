95cc3f6cf47c78a120f2dde12a53770a8639d042
sleepyfox97 <h1649bot@gmail.com>
2023-10-16 10:20:14 +0900

    リファクタリングした

--- problems/python/yokan_party.py
+++ problems/python/yokan_party.py
@@ -1,13 +1,18 @@
+def parse_input() -> [int, int, int, list[int]]:
+  n, l = map(int, input().split())
+  k = int(input())
+  li = list(map(int, input().split()))
+  li.append(l) # 末尾にlを追加しないと、最後の区間の長さが計算できない
+  li.insert(0,0)# 先頭に0を追加することで、checkをしやすくする
+  return n, l, k, li
+
 def check(mid: int, k: int, li: list[int]) -> bool:
   i: int = 0
   j: int = 0
   d: int = 0
-  for i in range(len(li)):
-    if (mid <= li[i]):
-      d = d + 1
-      j = i
-      break
-  
+  # この可能性は、入力値に的にありえないが、可読性のために用意しておく．
+  if mid > li[-1]:#一番後ろの要素を指定
+    return False
   for i in range(len(li)):
     if (mid <= li[i] - li[j]):
       d = d + 1
@@ -16,25 +21,21 @@ def check(mid: int, k: int, li: list[int]) -> bool:
       return True
   return False
 
-def main():
-  n, l = map(int, input().split())
-  k = int(input())
-  li = list(map(int, input().split()))
-  li.append(l) # 末尾にlを追加しないと、最後の区間の長さが計算できない
-
-  max: int = l // (k + 1) + 1 # N=1 ,L=4, K=1, A_1=2 の時にmid==min=1になってしまうのを防ぐ(31行目に関わってくる．もっと綺麗な書き方ありそう)
+def binary_serch(l, k, li) -> int:
+  max: int = l // (k + 1)
   min: int = 1
-  mid: int = (min + max) // 2
-
-  while min <= max:
-    if (check(mid, k + 1, li)):
+  while min < max:
+    mid = (min + max + 1) // 2  # N=1 ,L=4, K=1, A_1=2 の時にmid==min=1になってしまうのを防ぐための+1
+    if (check(mid, k + 1, li)): 
       min = mid
     else:
-      max = mid
-    old_mid = mid
-    mid =  (min + max) // 2
-    if mid == old_mid:
-      break
-  print(mid)
+      max = mid - 1
+  return min
+
+def main():
+  n, l, k, li = parse_input()
+  result = binary_serch(l, k, li)
+  print(result)
   
-main()
+if __name__=="__main__":
+  main()
